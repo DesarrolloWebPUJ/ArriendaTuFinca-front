c372b5ee983a8d84c119c1d2b1f60f7ed11a922c
Pablo Gonzalez <p-gonzalez@javeriana.edu.co>
2024-11-20 15:06:36 -0500

    Agregar Fotos front

--- src/app/common/components/propiedad-detalles/propiedad-detalles.component.ts
+++ src/app/common/components/propiedad-detalles/propiedad-detalles.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { BasePropiedadDTO } from '../../models/Propiedad/BasePropiedadDTO';
 import { CostoDineroPipe } from "../../pipes/costo-dinero.pipe";
+import { BasePropiedadDTO } from '../../models/Propiedad/BasePropiedadDTO';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-propiedad-detalles',
@@ -13,4 +16,38 @@ export class PropiedadDetallesComponent {
   @Input() propiedad: any;
   @Input() isArrendador: boolean = false;
   defaultPropiedadImagePath = '/assets/sample/propiedad2.jpg';
+  constructor(private http: HttpClient) {}
+  selectedFile: File | null = null;
+
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUploadPhoto(event: any): void {
+    event.preventDefault();
+    if (!this.selectedFile || !this.propiedad) {
+      console.error('No hay archivo seleccionado o propiedad cargada');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('photo', this.selectedFile);
+
+    this.http.post(`${environment.apiUrl}/api/propiedad/${this.propiedad.idPropiedad}/fotos`, formData)
+      .subscribe(response => {
+        console.log('Foto subida con éxito', response);
+        this.getPropiedad(this.propiedad.idPropiedad);  // Refrescar las fotos
+      }, error => {
+        console.error('Error al subir la foto', error);
+      });
+  }
+
+  getPropiedad(id: number): void {
+    this.http.get(`${environment.apiUrl}/api/propiedad/${id}`)
+      .subscribe((data: any) => {
+        this.propiedad = data;
+      }, error => {
+        console.error('Error al obtener la propiedad', error);
+      });
+  }
 }
